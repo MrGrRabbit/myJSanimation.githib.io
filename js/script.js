7137450a50e5fb5f2ab8151ca2ce3bd7166e1f11
MrGrRabbit <Mr.GrRabbit@yandex.ru>
2019-11-27 00:31:16 +0500

    Доработки

--- js/script.js
+++ js/script.js
@@ -15,15 +15,15 @@ function myMove() {
 }
 */
 //Пример анимации :
-corowV1.onclick = function() {
+cowV1.onclick = function() {
   let start = Date.now();
 
   let timer = setInterval(function() {
     let timePassed = Date.now() - start;
 
-    corowV1.style.left = timePassed / 5 + 'px';
+    cowV1.style.right = timePassed / 5 + 'px';
 
-    if (timePassed > 2000) clearInterval(timer);
+    if (timePassed > 3000) clearInterval(timer);
 
   }, 20);
 }
